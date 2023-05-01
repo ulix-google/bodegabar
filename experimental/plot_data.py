873ef287f7054fa012ef53810df6d25115bc6bf1
Ulysses Lizarraga <ulizarra@usc.edu>
2023-04-30 22:31:28 -0700

    Plot all data files

--- experimental/plot_data.py
+++ experimental/plot_data.py
@@ -3,14 +3,21 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import mplcyberpunk
 
+
+class DataFile:
+    def __init__(self, filename: str, plotColor: str) -> None:
+        self.plotColor = plotColor
+        self.filename = filename
+
+
 # Time period in seconds.
 time_period = 0.1
 data_files = [
-    "sunny_afternoon_direct_light.txt",
-    "sunny_afternoon_hard_blinds.txt",
-    "sunny_afternoon_light_blinds.txt",
-    "night_no_indoor_lights.txt",
-    "night_all_indoor_lights_on.txt",
+    DataFile("sunny_afternoon_direct_light.txt", "gold"),
+    DataFile("sunny_afternoon_hard_blinds.txt", "orange"),
+    DataFile("sunny_afternoon_light_blinds.txt", "lightyellow"),
+    DataFile("night_no_indoor_lights.txt", "violet"),
+    DataFile("night_all_indoor_lights_on.txt", "mediumpurple"),
 ]
 
 # Style it up.
@@ -20,12 +27,19 @@ plt.ylabel("ADC signal (0 to 65535)")
 
 sensor_data = defaultdict(list)
 for data_file in data_files:
-    with open(data_file) as file:
+    with open(data_file.filename) as file:
         for string_value in file:
-            sensor_data[data_file].append(float(string_value))
-    time_vector = np.arange(0.0, len(sensor_data[data_file]) * time_period, time_period)
+            sensor_data[data_file.filename].append(float(string_value))
+    time_vector = np.arange(
+        0.0, len(sensor_data[data_file.filename]) * time_period, time_period
+    )
     len(time_vector)
-    plt.plot(time_vector, sensor_data[data_file], label=data_file)
+    plt.plot(
+        time_vector,
+        sensor_data[data_file.filename],
+        label=data_file.filename,
+        color=data_file.plotColor,
+    )
 
 
 plt.xlim([0, 9])
